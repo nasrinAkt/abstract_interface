ce91c4282e80c5ecb38378709c2895a1d86b48b3
Nasrin <test@test.com>
2023-11-20 19:54:19 -0500

    added 2 class

---
+++ src/abstract1/class7.java
@@ -0,0 +1,28 @@
+package abstract1;
+
+public class class7 implements class6{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mon() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mon1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mon2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
